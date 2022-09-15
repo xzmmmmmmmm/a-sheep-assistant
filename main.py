545722f1f3fd74fcfa42f2c42bfcc80c21470a52
xzmmmmmmmm <106919218+xzmmmmmmmm@users.noreply.github.com>
2022-09-15 13:19:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 """
 # 接口地址,请求头参数必填:t
 map_api = "https://cat-match.easygame2021.com/sheep/v1/game/map_info?map_id=80001"
-finish_api = "https://cat-match.easygame2021.com/sheep/v1/game/game_over?rank_score=1&rank_state=1&rank_time=%s&rank_role=1&skin=1"
+finish_api = "https://cat-match.easygame2021.com/sheep/v1/game/game_over?rank_score=1&rank_state=1&rank_time=1200&rank_role=1&skin=1"
 
 
 # 完成闯关
